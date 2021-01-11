8d286c4d3107976ec05e67cca88cf2939973be6b
abhijato <68499000+abhijato@users.noreply.github.com>
2021-01-11 16:16:24 +0530

    add host

--- data/data/settings.py
+++ data/data/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'd=_v+)tegzqz+-^@di8j#_5vo7kpj7yo2se2vwkrj&74iz!24e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['datastorageheroku.herokuapp.com']
 
 
 # Application definition
